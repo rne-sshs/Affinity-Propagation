7dde2f860e0aa26a4b531bdc31b92007b686a6f4
andyjung <andyjung2104@gmail.com>
2022-05-26 20:34:30 +0900

    feat: make-3d-testcases

--- test/AP-Generator_andy.py
+++ test/AP-Generator_andy.py
@@ -1,12 +1,24 @@
 import random
 
-n=5000
-filename = 'okok'
+is_3d = True
+
+n=500
+filename = 'ap_3d_test2'
 maxcoor = 1000
+
 file = open(filename+'.txt','w')
 file.write(str(n)+"\n")
-for i in range(n):
-    r1=random.uniform(0,maxcoor)
-    r2=random.uniform(0,maxcoor)
-    file.write(f"{r1} {r2}\n")
-file.close()
+
+if is_3d:
+    for i in range(n):
+        r1=random.uniform(0,maxcoor)
+        r2=random.uniform(0,maxcoor)
+        r3=random.uniform(0,maxcoor)
+        file.write(f"{r1} {r2} {r3}\n")
+        
+else:
+    for i in range(n):
+        r1=random.uniform(0,maxcoor)
+        r2=random.uniform(0,maxcoor)
+        file.write(f"{r1} {r2}\n")
+    file.close()
