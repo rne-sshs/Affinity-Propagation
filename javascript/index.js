ae374fe833024387c20b34ff1dfa82dab4294eee
notdatum <notdatum@gmail.com>
2022-05-26 10:22:50 +0900

    refactor: del edited code

--- javascript/index.js
+++
@@ -1,6 +0,0 @@
-var apclust = require('./AffinityPropagation_seonu.js')
-
-var array = apclust.getDataArray()
-console.log(array)
-var result = apclust.getClusters(array, {preference: "median", damping: 0.5, maxIter: 50, convIter: 50})
-console.log(result)
